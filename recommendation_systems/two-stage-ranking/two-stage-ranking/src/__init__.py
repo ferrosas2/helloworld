99d60a58af26fb5d60ee32f02d73ee68fa9eeead
ferrosas2 <rosasfernando@hotmail.com>
2025-12-20 19:12:17 -0600

    Add two-stage-ranking LTR system to recommendation_systems/

---
+++ recommendation_systems/two-stage-ranking/two-stage-ranking/src/__init__.py
@@ -0,0 +1,7 @@
+"""
+Package initialization for Two-Stage Ranking System
+"""
+
+__version__ = "1.0.0"
+__author__ = "MLOps Engineer"
+__description__ = "Production XGBoost Learning-to-Rank for High-Scale Auctions"
